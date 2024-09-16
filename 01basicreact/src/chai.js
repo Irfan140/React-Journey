868be64ab94f0718a72078f82e3394227f0c580a
Irfan140 <irfanmehmud140@gmail.com>
2024-09-16 19:31:17 +0530

    another is added

---
+++ 01basicreact/src/chai.js
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h2>I am doing this using basicreact folder</h2>
+    )
+}
+
+export default Chai
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return (
+        <h3>Irfan mehmud</h3>
+    )
+}
+
+export default Chai
