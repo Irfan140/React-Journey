28356fe85644c54fb61b985ef0659cb8312a330a
Irfan140 <irfanmehmud140@gmail.com>
2024-12-07 19:59:00 +0530

    modified

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,18 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
-//  let myObj = {
-//   username: "irfan",
-//   age: 20
-//  }
+  /*
+  let myObj = {
+  username: "irfan",
+  age: 20
+ }
 
-//  let newArr = [1,2,3]
+  */
+ 
+ let newArr = [1,2,3]
 
   return (
     <>
