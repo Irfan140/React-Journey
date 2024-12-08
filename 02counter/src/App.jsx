88563031e541d451739a861871c90a50700eabfa
Irfan140 <irfanmehmud140@gmail.com>
2024-12-08 18:33:58 +0530

    comments added

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,34 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  
-let [counter,setCounter] = useState(0) // inside useState instead of 13 we can use any value..these value will go to counter variable as default value...and setCounter will be the method responsible for state change (counter ko change kr na ka responsibility) in the UI
+  let [counter, setCounter] = useState(0); // inside useState instead of 13 we can use any value..these value will go to counter variable as default value...and setCounter will be the method responsible for state change (counter ko change kr na ka responsibility) in the UI
 
-//  let counter = 0;
+  //  let counter = 0;
   const addValue = () => {
-    counter++
-    setCounter(counter)
-    
-  }
+    counter++;
+    setCounter(counter);
+  };
 
   const decreaseValue = () => {
-    setCounter(counter - 1)
-    
-  }
+    setCounter(counter - 1);
+  };
 
   return (
     <>
-    <h1>A simple counting project</h1>
-    <h2>Counter value: {counter}</h2>
-    <button onClick={addValue}>Add value</button>
-    <br />
-    <br />
-    <button onClick={decreaseValue}>Remove value</button>
+      <h1>A simple counting project</h1>
+      <h2>Counter value: {counter}</h2>
+      {/* To perform any action on any button we use onClick */}
+      <button onClick={addValue}>Add value</button>
+      <br />
+      <br />
+      <button onClick={decreaseValue}>Remove value</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
